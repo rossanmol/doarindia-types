a23eee52b445fa621b8ea7009c40ce5fe03ebb3d
Rostislav Dascal <rostislav.dascal@betssongroup.com>
2020-04-24 10:46:49 +0200

    export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./moderator.model";
+export * from "./feature-item.model";
+export * from "./feature-item.const";
 export * from "./backblaze.model";
 export * from "./thumbnail.model";
